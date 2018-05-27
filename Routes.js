4a703c63ed77b23a92308465ce6c0f17a8c36308
idandagan1 <idandagan1@gmail.com>
2018-05-27 23:55:59 +0300

    added eslint

--- Routes.js
+++ Routes.js
@@ -17,19 +17,19 @@ function setup(app, handlers, authorisationPolicy) {
     app.post('/api/:userId/pools/:poolId/games/:gameId', authorisationPolicy, handlers.bets.createOrUpdate);
 
     //app.post('/api/profiles/:userId/lists', authorisationPolicy, handlers.list.createShoppingList);
-	//app.post('/api/profiles/:userId/lists', authorisationPolicy, handlers.list.createShoppingList);
-	//app.post('/api/profiles/:userId/lists/:templateId', authorisationPolicy, handlers.list.createShoppingList);
-	//app.put('/api/profiles/:userId/lists/:shoppingListId', authorisationPolicy, handlers.list.updateShoppingList);
-	//app.get('/api/profiles/:userId/lists/:shoppingListId', authorisationPolicy, handlers.list.getShoppingList);
-	//app.get('/api/profiles/:userId/lists', authorisationPolicy, handlers.list.getShoppingLists);
-	//app.delete('/api/profiles/:userId/lists/:shoppingListId', authorisationPolicy, handlers.list.deleteShoppingList);
-	//app.post('/api/profiles/:userId/lists/:shoppingListId/item/', authorisationPolicy, handlers.list.addShoppingItem);
-	//app.put('/api/profiles/:userId/lists/:shoppingListId/item/:itemId', authorisationPolicy, handlers.list.updateShoppingItem);
-	//app.delete('/api/profiles/:userId/lists/:shoppingListId/item/:itemId', authorisationPolicy, handlers.list.deleteShoppingItem);
-	//app.put('/api/profiles/:userId/lists/:shoppingListId/item/:itemId/crossout', authorisationPolicy, handlers.list.crossoutShoppingItem);
-	//app.post('/api/auth/facebook/mobile', handlers.auth.facebookMobileLogin);
+    //app.post('/api/profiles/:userId/lists', authorisationPolicy, handlers.list.createShoppingList);
+    //app.post('/api/profiles/:userId/lists/:templateId', authorisationPolicy, handlers.list.createShoppingList);
+    //app.put('/api/profiles/:userId/lists/:shoppingListId', authorisationPolicy, handlers.list.updateShoppingList);
+    //app.get('/api/profiles/:userId/lists/:shoppingListId', authorisationPolicy, handlers.list.getShoppingList);
+    //app.get('/api/profiles/:userId/lists', authorisationPolicy, handlers.list.getShoppingLists);
+    //app.delete('/api/profiles/:userId/lists/:shoppingListId', authorisationPolicy, handlers.list.deleteShoppingList);
+    //app.post('/api/profiles/:userId/lists/:shoppingListId/item/', authorisationPolicy, handlers.list.addShoppingItem);
+    //app.put('/api/profiles/:userId/lists/:shoppingListId/item/:itemId', authorisationPolicy, handlers.list.updateShoppingItem);
+    //app.delete('/api/profiles/:userId/lists/:shoppingListId/item/:itemId', authorisationPolicy, handlers.list.deleteShoppingItem);
+    //app.put('/api/profiles/:userId/lists/:shoppingListId/item/:itemId/crossout', authorisationPolicy, handlers.list.crossoutShoppingItem);
+    //app.post('/api/auth/facebook/mobile', handlers.auth.facebookMobileLogin);
     app.post('/api/auth/login', handlers.auth.handleLoginRequest);
-	app.post('/api/auth/logout', authorisationPolicy, handlers.auth.logout);
+    app.post('/api/auth/logout', authorisationPolicy, handlers.auth.logout);
 }
 
 exports.setup = setup;
