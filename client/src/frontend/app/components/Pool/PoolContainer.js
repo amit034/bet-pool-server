f0482a40cc86724032a179676e6bd9d18ec9df63
amit034 <amitr@perion.com>
2021-10-25 02:16:17 +0300

    add use effects

--- client/src/frontend/app/components/Pool/PoolContainer.js
+++ client/src/frontend/app/components/Pool/PoolContainer.js
@@ -1,75 +1,48 @@
-import React from 'react';
+'use strict';
+import React, {useEffect} from 'react';
 import _ from 'lodash';
-import {withRouter} from 'react-router-dom';
-import {getPoolParticipates, getUserBets, updateUserBet, updateUserBets} from '../../actions/pools';
+import {useHistory} from 'react-router-dom';
+import  {getUserBets, updateUserBet} from '../../actions/pools';
 import NavigationMenu from './NavigationMenu';
-import {clearFocused, setFocused} from '../../actions/betPad';
-import {connect} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import GameList from './GameList/GameList';
-import BetPad from './GameList/BetPad';
 
-class PoolContainer extends React.Component{
-  constructor(props){
-    super(props);
-      this.submitBets = this.submitBets.bind(this);
-      this.onBetChange = this.onBetChange.bind(this);
-      this.onBetKetChange = this.onBetKetChange.bind(this);
-      this.onBetFocused= this.onBetFocused.bind(this);
-      this.onShowOthers = this.onShowOthers.bind(this);
-      this.state ={
-          updates: {}
-      }
-  }
-  // Lifecycle method
+const PoolContainer = (props) => {
+    const dispatch = useDispatch();
+    let history = useHistory();
+    useEffect(() => {
+        const {match, socket} = props;
+        const poolId = match.params.id;
+        dispatch(getUserBets(poolId));
+        socket.emit('joinPool', poolId);
+        return () => {
+            const {match, socket} = props;
+            const poolId = match.params.id;
+            socket.emit('leavePool', poolId);
+        };
+    }, [dispatch]);
 
-  componentDidMount(){
-      const {dispatch, match, socket} = this.props;
-      const poolId = match.params.id;
-      dispatch(getUserBets(poolId));
-      socket.emit('joinPool', poolId);
-      //dispatch(getPoolParticipates(poolId));
-  }
-  componentWillUnmount() {
-      const {match, socket} = this.props;
-      const poolId = match.params.id;
-      socket.emit('leavePool', poolId);
-  }
-
-  onBetKetChange(challengeId, key, value){
-        const bet = _.get(this.props.bets, challengeId);
+    function onBetKetChange(challengeId, key, value) {
+        const bet = _.get(props.bets, challengeId);
         _.set(bet, key, value);
-        this.props.dispatch(updateUserBet(this.props.match.params.id , challengeId, bet))
-
-  }
-   onBetChange(challengeId, updatedBet){
-        const bet = _.get(this.props.bets, challengeId);
-        _.assign(bet, _.pick(updatedBet, ['score1', 'score2']));
-        this.props.dispatch(updateUserBet(this.props.match.params.id , challengeId, bet))
-  }
-  onShowOthers(challengeId){
-      this.props.history.push(`/pools/${this.props.match.params.id}/challenges/${challengeId}/participates`);
-  }
-  onBetFocused(e, betId, betFieldName){
-      this.props.dispatch(setFocused(betId, e.target.value, e.target.offsetTop, betFieldName));
-  }
-  submitBets(){
-      this.props.dispatch(updateUserBets(this.props.match.params.id, this.props.bets));
-  }
-
-  render(){
-    return (<div id="content" className="ui container">
-        <GameList poolId={this.props.match.params.id} onBetChange={this.onBetChange} onBetKeyChange={this.onBetKetChange} onShowOthers ={this.onShowOthers} onBetFocused={this.onBetFocused} />
-        <BetPad focused={this.props.focused} onBetChange={this.onBetChange}/>
-        <NavigationMenu  />
-    </div>);
-
-  }
-}
-
-function mapStateToProps({pools: {bets}}) {
-    return {
-        bets
+        dispatch(updateUserBet(props.match.params.id, challengeId, bet));
     }
-}
 
-export default withRouter(connect((mapStateToProps))(PoolContainer));
+    function onBetChange(challengeId, updatedBet) {
+        const bet = _.get(props.bets, challengeId);
+        _.assign(bet, _.pick(updatedBet, ['score1', 'score2']));
+        dispatch(updateUserBet(props.match.params.id, challengeId, bet));
+    }
+
+    function onShowOthers(challengeId) {
+        history.push(`/pools/${props.match.params.id}/challenges/${challengeId}/participates`);
+    }
+
+    return (<div id="content" className="ui container">
+                <GameList poolId={props.match.params.id} onBetChange={onBetChange}
+                          onBetKeyChange={onBetKetChange}
+                          onShowOthers={onShowOthers} />
+                <NavigationMenu/>
+            </div>);
+}
+export default PoolContainer;
