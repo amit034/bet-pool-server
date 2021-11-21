048f835c7a0745f347cceb71707b43e60333559f
idanlevi507 <idanlevi15@gmail.com>
2021-11-21 16:50:59 +0200

    Revert "fixing ranking - before first"
    This reverts commit cfa9287f2cf02b4da8cdc0fdc1faf98211cddeb4.

--- client/src/frontend/app/utils.js
+++ client/src/frontend/app/utils.js
@@ -2,38 +2,18 @@
 import _ from 'lodash';
 
 export function getParticipatesWithRank(participates) {
-    if (_.isEmpty(participates)) {
+    if(_.isEmpty(participates)) {
         return {};
     }
     const [first, ...others] = _.orderBy(participates, ['score', 'medals.3', 'medals.2', 'medals.1'], ['desc', 'desc', 'desc', 'desc']);
-    const { participatesWithRank } = _.reduce(others, (agg, participate, idx) => {
+    const {participatesWithRank} = _.reduce(others, (agg, participate) => {
         if (!_.isEqual(participate.medals, agg.medals)) {
-            agg.rank = idx + 2;
+            agg.rank++;
             agg.medals = participate.medals;
         }
-        agg.participatesWithRank.push(_.assign(participate, { rank: agg.rank }));
+        agg.participatesWithRank.push(_.assign(participate, {rank: agg.rank}));
         return agg;
-    }, { participatesWithRank: [_.assign({}, first, { rank: 1 })], rank: 1, medals: first.medals });
+    }, {participatesWithRank: [_.assign({}, first, {rank: 1})], rank: 1, medals: first.medals});
     return participatesWithRank;
 }
 
-export function getParticipatesWithRank1(participates) {
-    if (_.isEmpty(participates)) {
-        return {};
-    }
-    const [first, ...others] = _.orderBy(participates, ['score', 'medals.3', 'medals.2', 'medals.1'], ['desc', 'desc', 'desc', 'desc']);
-    const { participatesWithRank } = _.reduce(others, (agg, participate) => {
-        if (_.isEqual(participate.medals, agg.medals)) {
-            agg.participatesWithRank.push(_.assign(participate, { rank: agg.rank }));
-            agg.rank++;
-        }
-        else {
-            agg.rank++;
-            agg.medals = participate.medals;
-            agg.participatesWithRank.push(_.assign(participate, { rank: agg.rank }));
-        }
-        return agg;
-
-    }, { participatesWithRank: [_.assign({}, first, { rank: 1 })], rank: 1, medals: first.medals });
-    return participatesWithRank;
-}
